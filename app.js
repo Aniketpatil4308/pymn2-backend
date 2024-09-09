d755bd217d88746f804169c3dfa0c36b95e6c347
aniket <aniketpatil108108@gmail.com>
2024-09-09 12:25:50 +0530

    added new files

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'] 
+}));
 
 app.use(express.json());
 
